e68902bb57f434370e10799647c540b9919176c4
nikollllllas <nikollasohta2014@gmail.com>
2024-03-13 15:32:15 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Transaction } from './pages/transactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+
+      <Transaction />
     </ThemeProvider>
   )
 }
---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from '../../components/header'
+
+export function Transaction() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
