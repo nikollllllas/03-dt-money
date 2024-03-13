40a5f0b1e9ec877c445da07c983927ceadeb0718
nikollllllas <nikollasohta2014@gmail.com>
2024-03-13 15:54:14 -0300

    feat: summary

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/header'
+import { Summary } from '../../components/summary'
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
