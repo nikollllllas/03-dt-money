492ab7be6facc3e32e30b78e651dc8703246fde0
nikollllllas <nikollasohta2014@gmail.com>
2024-03-15 15:55:14 -0300

    feat: calc summary

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -10,6 +10,25 @@ import { TransactionsContext } from '../../contexts/transactionscontext'
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -22,7 +41,7 @@ export function Summary() {
           />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -35,7 +54,7 @@ export function Summary() {
           />
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant='green'>
@@ -48,7 +67,7 @@ export function Summary() {
           />
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
